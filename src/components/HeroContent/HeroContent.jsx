4aedd35a87f7f70c49897e1f3b580adc87a1b1c1
unknown <skubidu2@inbox.ru>
2022-02-23 18:04:27 +0400

    sdfsdfsdfs

---
+++ src/components/HeroContent/HeroContent.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './HeroContent.css'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+const HeroContent = () => {
+  return (
+    <div className="hero_content">
+    <section>
+       <h3>HEAD OUT ON YOUR NEXT<br/>ADVENTURE</h3> 
+       <h4>from Batumi Georgia</h4>
+    </section>
+    <div className="soc_media_wrapper">
+        <FacebookIcon style={{color: 'white'}}/>
+        <InstagramIcon style={{color: 'white'}}/>
+        <YouTubeIcon style={{color: 'white'}}/>
+        <PinterestIcon style={{color: 'white'}}/>
+    </div>
+    </div>
+  )
+}
+
+export default HeroContent
